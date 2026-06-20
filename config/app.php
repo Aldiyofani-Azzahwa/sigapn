fc937879c4256f41fe301c659749d1d0459db633
Aldiyofani-Azzahwa <aldiyofaniazzahwa@gmail.com>
2026-06-20 20:05:01 +0700

    Register Laravel default app providers

--- config/app.php
+++ config/app.php
@@ -1,15 +1,42 @@
 <?php
 
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\ServiceProvider;
+
 return [
     'name' => env('APP_NAME', 'SIGAPN'),
+
     'env' => env('APP_ENV', 'production'),
+
     'debug' => (bool) env('APP_DEBUG', false),
+
     'url' => env('APP_URL', 'http://localhost'),
+
     'timezone' => env('APP_TIMEZONE', 'Asia/Jakarta'),
+
     'locale' => env('APP_LOCALE', 'id'),
+
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+
     'faker_locale' => env('APP_FAKER_LOCALE', 'id_ID'),
+
     'key' => env('APP_KEY'),
+
     'cipher' => 'AES-256-CBC',
-    'maintenance' => ['driver' => 'file'],
+
+    'maintenance' => [
+        'driver' => 'file',
+    ],
+
+    'providers' => ServiceProvider::defaultProviders()
+        ->merge([
+            App\Providers\AppServiceProvider::class,
+        ])
+        ->toArray(),
+
+    'aliases' => Facade::defaultAliases()
+        ->merge([
+            //
+        ])
+        ->toArray(),
 ];
