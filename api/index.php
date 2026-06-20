b48d5186b84a9d1a152f6a493f9f71f02cd37fce
Aldiyofani-Azzahwa <aldiyofaniazzahwa@gmail.com>
2026-06-20 19:56:08 +0700

    Show Laravel error for Vercel debug

--- api/index.php
+++ api/index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 if (! is_dir('/tmp/views')) {
     mkdir('/tmp/views', 0777, true);
 }
@@ -11,4 +15,15 @@ if (! is_dir('/tmp/cache')) {
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
 $_SERVER['VIEW_COMPILED_PATH'] = '/tmp/views';
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (Throwable $e) {
+    http_response_code(500);
+
+    echo '<pre style="white-space: pre-wrap; font-family: monospace;">';
+    echo 'ERROR CLASS: ' . get_class($e) . PHP_EOL;
+    echo 'MESSAGE: ' . $e->getMessage() . PHP_EOL . PHP_EOL;
+    echo 'FILE: ' . $e->getFile() . ':' . $e->getLine() . PHP_EOL . PHP_EOL;
+    echo $e->getTraceAsString();
+    echo '</pre>';
+}
